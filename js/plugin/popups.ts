2f1f0151b580e4c3fa20f86d1d626541c3a6939c
Tom Bruno <tom.bruno@smartqasa.com>
2024-09-10 18:28:16 +0000

    Refactor popup styles for better alignment and padding

--- js/plugin/popups.ts
+++ js/plugin/popups.ts
@@ -218,14 +218,14 @@ class BrowserModPopup extends LitElement {
                 ${this.title
                     ? html`
                           <ha-dialog-header slot="heading">
-                              <span slot="title" .title="${this.title}">${this.title}</span>
                               ${this.dismissable
                                   ? html`
-                                        <ha-icon-button dialogAction="cancel" class="dismiss-icon">
+                                        <ha-icon-button dialogAction="cancel" slot="navigation">
                                             <ha-icon .icon=${"mdi:close"}></ha-icon>
                                         </ha-icon-button>
                                     `
                                   : ""}
+                              <span slot="title" .title="${this.title}">${this.title}</span>
                           </ha-dialog-header>
                       `
                     : html``}
@@ -335,24 +335,12 @@ class BrowserModPopup extends LitElement {
                 display: flex;
             }
 
-            ha-dialog-header {
-                display: flex;
-                align-items: center;
-                justify-content: space-between;
-                padding: 16px;
-            }
-
             ha-dialog-header > span {
                 font-size: var(--sq-dialog-header-font-size, 24px);
                 font-weight: var(--sq-dialog-header-font-weight, 300);
                 overflow: hidden;
                 text-overflow: ellipsis;
                 white-space: nowrap;
-                flex: 1; /* Allow the title to take up remaining space */
-            }
-
-            .dismiss-icon {
-                margin-left: 16px; /* Space between title and button */
             }
 
             :host([wide]) ha-dialog {
